154b926a06350a989944e4e286cff444f261a6af
abdijalil <abdijalilimam@gmail.com>
2025-10-07 22:04:02 -0400

    day 01

---
+++ day01.py
@@ -0,0 +1,11 @@
+#Day 01 exercise 
+
+name = input("what is your name?")
+age = input ("How old are you?")
+
+
+#turn agei into an integer
+age = int(age)
+
+print (f"Hello, {name}!" )
+print (f"Next year you will be {age + 1 } years old")
