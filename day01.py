428dfeff621b8e2df10b12e0dc43535d8e4d8a70
abdijalil <abdijalilimam@gmail.com>
2025-10-09 20:15:58 -0400

    finished day01 of real exercises

--- day01.py
+++ day01.py
@@ -1,11 +1,30 @@
-#Day 01 exercise 
+#-------------------------------------------------
+#Day 01 
+#print ("Welcome to Python Bootcamp!")
+#print ("Let's get to know you...")
 
-name = input("what is your name?")
-age = input ("How old are you?")
+
+# Ask the user for their name
+#name = input ("What is your name?")
+
+#Ask for age 
+#age = input("How old are you?")
+
+#Print a response using veriables 
+#print("Nice to meet you," + name + "!")
+#print("You are " + age + "years old!")
+
+#birth = 2025 - int(age)
+#print("Wow! so you were born around the year " + str(birth) + "!")
+#---------------------------------------------------
 
 
-#turn agei into an integer
-age = int(age)
+#-----------------------mini challenge-----------------------
 
-print (f"Hello, {name}!" )
-print (f"Next year you will be {age + 1 } years old")
+print("Welcome!")
+name = input("What is your name?")
+color = input("what is your favorite color?")
+hobby = input("what hobby you enjoy")
+
+
+print("Hi " + str(name)+ "!" + " You love the color" + str(color) + " and enjoy" + str(hobby)+ ".")
---
+++ day02.py
@@ -0,0 +1,11 @@
+#Day 02 excise 
+
+#get user input 
+name = input("what is your name?")
+age = input ("How old are you?")    
+
+#turn birth year into an integer 
+
+birth_year = 2025 - int(age)
+# print the greating 
+print (f"Hello, {name}, you were born in {birth_year}")
